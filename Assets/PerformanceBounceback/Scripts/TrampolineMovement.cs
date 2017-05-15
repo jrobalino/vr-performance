7a50ceedf8a2298a87846522792162b2ba105531
J Robalino <J Robalino>
2017-05-14 19:14:08 -0500

    refactor: Optimize trampoline rigidbody motion.

--- Assets/PerformanceBounceback/Scripts/TrampolineMovement.cs
+++ Assets/PerformanceBounceback/Scripts/TrampolineMovement.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class TrampolineMovement : MonoBehaviour {
 
-    public Vector3 direction = new Vector3(1,0,0);
+    public Vector3 direction = Vector3.right;
     public float moveSpeed = 3.5f;
     public float moveTime = 3f;
     private float time;
-	
+	Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate()
+	{
+		rb.MovePosition(transform.position + direction * Time.deltaTime * moveSpeed);
+	}
+
+
 	// Update is called once per frame
 	void Update () {
         time += Time.deltaTime;
@@ -17,6 +28,6 @@ public class TrampolineMovement : MonoBehaviour {
             time = 0;
             direction = direction * -1;
         }
-        transform.position += direction * Time.deltaTime * moveSpeed;		
+        //transform.position += direction * Time.deltaTime * moveSpeed;		
 	}
 }
